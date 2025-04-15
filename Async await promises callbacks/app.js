e5a8f16934740a25685d3810165e8bfed9606eb9
Vijit Verma <143703788+Vijitverm1234@users.noreply.github.com>
2025-04-16 01:30:09 +0530

    Add files via upload

---
+++ Async await promises callbacks/app.js
@@ -0,0 +1,12 @@
+const data=[
+    {name:"ajay",
+       profession: "Engineer"
+    },
+    {name:"yuvraj",
+        profession: "Engineer 1"
+     },
+     {name:"ti amo",
+        profession: "luvGuru "
+     },
+]
+console.log(data)
